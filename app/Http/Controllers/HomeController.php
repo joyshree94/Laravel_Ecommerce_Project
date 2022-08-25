b7ea8f8430e3553e127751554b145e7d4d7f6885
joyshree <joyshreemozumder94@gmail.com>
2022-08-25 18:17:37 +0600

    making view for card payment and paying using card to order product from cart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,6 +9,9 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\Cart;
 use App\Models\Order;
+
+use Session;
+use Stripe;
 class HomeController extends Controller
 {
     public function index()
@@ -132,5 +135,58 @@ class HomeController extends Controller
         return redirect()->back()->with('message','we have recieved order your order successfully.we will connect with you soon');
     }
 
+    public function stripe($totalprice)
+    {
+        return view('home.stripe',compact('totalprice'));
+    }
+    public function stripePost(Request $request, $totalprice)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+    
+        Stripe\Charge::create ([
+                "amount" => $totalprice *100,
+                "currency" => "usd",
+                "source" => $request->stripeToken,
+                "description" => "Thanks for payment" 
+        ]);
+        $user=Auth::user()->id;
+        $cart=Cart::where('user_id','=',$user)->get();
+        foreach( $cart as $data)
+        {
+            $order=new Order;
+            $order->name = $data->name;
+
+            $order->email=$data->email;
+
+            $order->phone=$data->phone; 	 	 	 	 	
+
+            $order->address=$data->address;
+
+            $order->user_id=$data->user_id;
+
+            $order->product_title=$data->product_title;
+
+            $order->quantity=$data->quantity;
+
+            $order->price=$data->price;
+
+            $order->image=$data->image;
+
+            $order->product_id=$data->product_id;
+
+            $order->payment_status= 'paid';
+
+            $order->delivery_status= 'processing';
+            $order->save();
+            $cart_id=$data->id;
+            $cart_item=Cart::find($cart_id);
+            $cart_item->delete();
+       
+        }
+      
+        Session::flash('success', 'Payment successful!');
+              
+        return back();
+    }
 }
    
