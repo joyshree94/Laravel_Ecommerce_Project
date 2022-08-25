5e23d32ef69e7da13d7c13b312ae14ae1cee787e
joyshree <joyshreemozumder94@gmail.com>
2022-08-25 18:39:03 +0600

    modify the card payment page

--- resources/views/home/stripe.blade.php
+++ resources/views/home/stripe.blade.php
@@ -1,15 +1,30 @@
 <!DOCTYPE html>
 <html>
 <head>
+<base href="/public">
     <title></title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="shortcut icon" href="images/favicon.png" type="">
+    <title>Famms - Fashion HTML Template</title>
+    <!-- bootstrap core css -->
+    <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+    <!-- font awesome style -->
+    <link href="home/css/font-awesome.min.css" rel="stylesheet" />
+    <!-- Custom styles for this template -->
+    <link href="home/css/style.css" rel="stylesheet" />
+    <!-- responsive style -->
+    <link href="home/css/responsive.css" rel="stylesheet" />
 </head>
 <body>
-    
+<div class="hero_area">
+        <!-- header section strats -->
+        <span style="font-size:18px;">  @include('home.header')</span>
+      
+
 <div class="container">
     
-    <h1>Pay Using Your Card- total amount ${{ $totalprice }}</h1>
+    <h1 style="font-size: 30px;text-align:center;padding-bottom:10px;">Pay Using Your Card- total amount ${{ $totalprice }}</h1>
     
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
@@ -78,7 +93,8 @@
     
                         <div class="row">
                             <div class="col-xs-12">
-                                <button class="btn btn-primary btn-lg btn-block" type="submit">Pay Now</button>
+                                <button class="" type="submit">Pay Now</button>
+                                <input type="submit" name="" value="Pay Now">
                             </div>
                         </div>
                             
@@ -162,4 +178,12 @@ $(function() {
      
 });
 </script>
+
+        <script src="home/js/jquery-3.4.1.min.js"></script>
+      <!-- popper js -->
+      <script src="jhome/s/popper.min.js"></script>
+      <!-- bootstrap js -->
+      <script src="home/js/bootstrap.js"></script>
+      <!-- custom js -->
+      <script src="home/js/custom.js"></script>
 </html>
